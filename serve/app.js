fe46524257ce04a783962dfdc3ac0edd8e818cd7
huangguozhen <huangguozhen@outlook.com>
2019-03-18 23:11:30 +0800

    add serve.

---
+++ serve/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+// const io = require('socket.io').listen(server);
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+server.listen(9000, function () {
+    console.log('node server running. http://localhost:9000');
+});
